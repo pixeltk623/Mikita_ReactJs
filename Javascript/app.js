6f5e62f914c34d8e5e82877f62e6f9273258dd01
Sharvan Kumar <info@pixeltk.com>
2022-03-07 09:31:40 +0530

    Done

---
+++ Javascript/app.js
@@ -0,0 +1,50 @@
+/*
+	alert("Hello This is Kumar");
+	console.log("Sharvan Kumar");
+*/
+/*Rules
+
+Variable names cannot contain spaces.
+
+Variable names must begin with a letter, an underscore (_) or a dollar sign ($).
+
+Variable names can only contain letters, numbers, underscores, or dollar signs.
+
+Variable names are case-sensitive.
+
+Certain words may not be used as variable names, because they have other meanings
+within JavaScript. Check out this complete list of the reserved words.*/
+
+// Camcel Case
+var listOfUsers = "Sharvan";
+// Pascal Case 
+
+var ListOfUsers = "Sanjeev";
+
+// Snake Case
+
+var list_of_users = "Hello";
+
+// console.log(list_of_users);
+// console.log(ListOfUsers);
+
+// console.log(listOfUsers);
+
+// var 45$listOfUsers = "sdsdds";
+
+// console.log(45$listOfUsers);
+
+var webUrl = "http://google.com/";
+var facebookUrl = "https://www.facebook.com/";
+var price = "546546";
+var totalPrice = 65.3265;
+
+console.log(webUrl);
+
+console.log(webUrl +" "+ facebookUrl);
+
+console.log( typeof(webUrl) )
+console.log( typeof(price) );
+console.log(typeof(totalPrice));
+
+console.log(price+totalPrice);
