ddba5c6d37dcb9150be59a74577c8c78e9ecc795
Dhrupad6054 <mahimiku15@gmail.com>
2022-05-13 08:11:51 +0530

    done

--- Class_object/index.js
+++ Class_object/index.js
@@ -1,1 +1,31 @@
-console.log('Hello');
+
+
+    constructor(Username, Email, Mobile, Address)
+    this.Username = Username;
+    this.Email = Email;
+    this.Address = Address;
+    
+    // getUserName() {
+    //     return this.Username;
+    // }
+
+    
+    // getEmail() {
+    //      return this.email;
+    // }
+
+    // getMobile() {
+    //    return this.mobile;
+    // }
+
+    
+    // getAddress() {
+    //      return this.address;
+    // }
+
+    // getFullDetails() {
+
+    //    return `My Name is ${this.Username} and emailId is ${this.email}`;
+    // }
+    
+    
